30e12bd395ea268121ea95a1c0e3a6fb79f6506e
Yuguang Tong <ygtong@berkeley.edu>
2015-03-14 10:07:08 -0700

    add one test to ALU; need more unit tests

--- test-files/autograder_limited.py
+++ test-files/autograder_limited.py
@@ -8,6 +8,7 @@ from autograder_base import file_locations, AbsoluteTestCase, FractionalTestCase
 tests = [
   ("ALU addu test",AbsoluteTestCase(os.path.join(file_locations,'ALU-addu.circ'),os.path.join(file_locations,'out-files/ALU-addu.out'),1)),
   ("ALU shift test",AbsoluteTestCase(os.path.join(file_locations,'ALU-shift.circ'),os.path.join(file_locations,'out-files/ALU-shift.out'),1)),
+  ("ALU add test",AbsoluteTestCase(os.path.join(file_locations,'ALU-add.circ'),os.path.join(file_locations,'out-files/ALU-add.out'),1)),
   ("RegFile insert test",AbsoluteTestCase(os.path.join(file_locations,'reg-insert.circ'),os.path.join(file_locations,'out-files/reg-insert.out'),1)),
 ]
 
