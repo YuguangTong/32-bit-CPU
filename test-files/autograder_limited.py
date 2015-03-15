42cc7c7d0758e581151f9f90a0dd51f26754bc58
Yuguang Tong <ygtong@berkeley.edu>
2015-03-14 17:28:43 -0700

    Add slt, sltu tests. Tidy up tunnel names in regfile.circ

--- test-files/autograder_limited.py
+++ test-files/autograder_limited.py
@@ -10,6 +10,7 @@ tests = [
   ("ALU shift test",AbsoluteTestCase(os.path.join(file_locations,'ALU-shift.circ'),os.path.join(file_locations,'out-files/ALU-shift.out'),1)),
   ("ALU add test",AbsoluteTestCase(os.path.join(file_locations,'ALU-add.circ'),os.path.join(file_locations,'out-files/ALU-add.out'),1)),
   ("ALU sub test",AbsoluteTestCase(os.path.join(file_locations,'ALU-sub.circ'),os.path.join(file_locations,'out-files/ALU-sub.out'),1)),
+  ("ALU slt test",AbsoluteTestCase(os.path.join(file_locations,'ALU-slt.circ'),os.path.join(file_locations,'out-files/ALU-slt.out'),1)),
   ("ALU bitpal/lfsr test",AbsoluteTestCase(os.path.join(file_locations,'ALU-bitpal-lfsr.circ'),os.path.join(file_locations,'out-files/ALU-bitpal-lfsr.out'),1)),
   ("RegFile insert test",AbsoluteTestCase(os.path.join(file_locations,'reg-insert.circ'),os.path.join(file_locations,'out-files/reg-insert.out'),1)),
 ]
