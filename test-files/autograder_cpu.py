64526f0ede5a19a419992afa570ef30f6649accf
William Huang <william.w.huang@gmail.com>
2015-03-21 00:29:03 -0700

    Released more tests.

--- test-files/autograder_cpu.py
+++ test-files/autograder_cpu.py
@@ -11,6 +11,9 @@ tests = [
   ("CPU lui test",AbsoluteTestCase(os.path.join(file_locations,'CPU-lui.circ'),os.path.join(file_locations,'out-files/CPU-lui.out'),1)),
   ("CPU ori test",AbsoluteTestCase(os.path.join(file_locations,'CPU-ori.circ'),os.path.join(file_locations,'out-files/CPU-ori.out'),1)),
   ("CPU slt test",AbsoluteTestCase(os.path.join(file_locations,'CPU-slt.circ'),os.path.join(file_locations,'out-files/CPU-slt.out'),1)),
+  ("CPU jr test",AbsoluteTestCase(os.path.join(file_locations,'CPU-jr.circ'),os.path.join(file_locations,'out-files/CPU-jr.out'),1)),
+  ("CPU halt test",AbsoluteTestCase(os.path.join(file_locations,'CPU-halt.circ'),os.path.join(file_locations,'out-files/CPU-halt.out'),1)),
+  ("CPU branches test",AbsoluteTestCase(os.path.join(file_locations,'CPU-branches.circ'),os.path.join(file_locations,'out-files/CPU-branches.out'),1)),
 ]
 
 if __name__ == '__main__':
